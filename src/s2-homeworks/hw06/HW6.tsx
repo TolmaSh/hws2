479921e4a042a97de2f8caee6070af2e974a4f5b
TolmaSh <m0v4anua@gmail.com>
2022-12-24 17:42:27 +0200

    feat: hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,6 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
+       setValue(restoreState('hw6-editable-span-value',''))
         // делают студенты
 
     }
@@ -35,7 +36,7 @@ const HW6 = () => {
                         onChangeText={setValue}
                         spanProps={{
                             id: 'hw6-editable-span',
-                            defaultText: 'enter text...',
+                            defaultText: 'Edit text...',
                         }}
                     />
                 </div>
